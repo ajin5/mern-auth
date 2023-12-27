4611f537e04f1ac790774231136bae8dca706857
ajin5 <ajinmla94@gmail.com>
2023-12-27 12:54:54 +0530

    signup page UI

--- api/index.js
+++ api/index.js
@@ -25,12 +25,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
-app.use(err, req, res, next){
-  const statuscode = err.statuscode || 500;
-  const message = err.message || 'internal server error';
-  return res.status(statuscode).json({
-    success:false,
-    error:message,
-    statuscode:statuscode,
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
   });
-};
+});
