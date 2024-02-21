54f55a061ff96279992abd67219628232d4da0c1
ajin5 <ajinmla94@gmail.com>
2024-02-21 13:03:38 +0530

    chsnges

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,8 +24,8 @@ export default function Header() {
                 className="h-7 w-7 rounded-full object-cover"
               />
             ) : (
-              <Link to="/sign-in">Sign In</Link>
-              // <li>Sign in</li>
+              // <Link to="/sign-in">Sign In</Link>
+              <li>Sign in</li>
             )}
           </Link>
         </ul>
